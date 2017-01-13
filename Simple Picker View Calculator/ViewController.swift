bb4cd8f4dd4a2b34c1168bf103a9640527125e35
ablake <student@ablake.local>
2017-01-13 10:28:48 -0600

    This may have some errors on it.

--- Simple Picker View Calculator/ViewController.swift
+++ Simple Picker View Calculator/ViewController.swift
@@ -11,10 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var pickerView: UIPickerView!
+    @IBOutlet weak var pickerViewTwo: UIPickerView!
+    @IBOutlet weak var pickerViewThree: UIPickerView!
+    @IBOutlet weak var pickerViewFour: UIPickerView!
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
-        
+        pickerView = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."]
+        pickerViewTwo = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
+        pickerViewThree = ["Jan.", "Feb.", "Mar.", "Apr.", "May", "June", "July", "Aug.", "Sept.", "Oct.", "Nov.", "Dec."]
+        pickerViewFour = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
     }
 }
